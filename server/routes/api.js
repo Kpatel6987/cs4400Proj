f80bc7c99f74803028bcb344caab0ce89f3d81ac
Ryan Gallaway <rgallaway@gatech.edu>
2017-04-07 13:35:10 -0400

    set up connection to 4400 database

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const mysql           = require("mysql");
 
 const con = mysql.createConnection({
-  host: "https://academic-mysql.cc.gatech.edu/cs4400_30",
+  host: "academic-mysql.cc.gatech.edu",
   user: "cs4400_30",
   password: "hBMdGbRb",
   port: "3306",
   database: "cs4400_30"
+});
+
+con.connect(function(err) {
+    if (err) {
+        console.log("Failed to connect to 4400 DB");
+        return;
+    }
+    console.log("Successfully connected to 4400 DB");
 });
 
 /* GET api listing. */
