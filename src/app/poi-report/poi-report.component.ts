01ac6057622b44549010cd3a4c7f4d3486496bb5
Hayden Flinner <hayden@gatech.edu>
2017-04-14 13:14:09 -0400

    Added remaining components.

---
+++ src/app/poi-report/poi-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poi-report',
+  templateUrl: './poi-report.component.html',
+  styleUrls: ['./poi-report.component.css']
+})
+export class PoiReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
