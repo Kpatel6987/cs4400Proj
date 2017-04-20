5e7c268000204a8abe3e5350d1a0602cf851c723
Kavish Patel <kpatel6987@gmail.com>
2017-04-19 22:10:05 -0400

    Commented SQL queries

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,55 +18,13 @@ con.connect(function(err) {
     console.log("Successfully connected to 4400 DB");
 });
 
-router.get("/cityOfficialList", function(req, res) {
-    con.query('SELECT * FROM CityOfficial',function(err,rows) {
-    if(err)
-        console.log("Error Selecting : %s ",err );
-        res.json(rows);
-        });
-});
 
-router.post("/addUser", function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    var email = req.body.email;
-    var userType = req.body.userType;
-    con.query("INSERT INTO User VALUES (?, ?, ?, ?)",
-     [username, email, password, userType], function(err, resp) {
-        if (err) {
-            console.log("Error " + err);
-        }
-        res.json(resp);
-     });
-});
-
-router.post("/addCityOfficial", function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    con.query("INSERT INTO CityOfficial VALUES (?, ?, ?, ?, ?)",
-     [username, req.body.title, false, req.body.state, req.body.state], function(err, resp) {
-        if (err) {
-            console.log("Error " + err);
-        }
-        res.json(resp);
-     });
-});
-
-router.get("/getUser", function(req, res) {
-    var username = req.query.username;
-    var password = req.query.password;
-    var obj = { status: false};
-    con.query("SELECT * FROM User WHERE username = ? and password = ?",
-        [username, password], function(err, response){
-        if (err)
-            res.json(err);
-        res.json(response);
-    });
-});
-
+/***************************************************************************** 
+                        VALIDATION ENDPOINTS
+******************************************************************************/
 router.get("/checkUser", function(req, res) {
     var username = req.query.username;
-    con.query("SELECT * FROM User WHERE username = ?",
+    con.query("SELECT * FROM User WHERE Username = ?",
         username, function(err, response){
         if (err)
             res.json(err);
@@ -93,6 +51,56 @@ router.get("/checkCityState", function(req, res) {
     });
 });
 
+/***************************************************************************** 
+                        REGISTRATION ENDPOINTS
+******************************************************************************/
+router.post("/addUser", function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+    var userType = req.body.userType;
+    con.query("INSERT INTO User VALUES (?, ?, ?, ?)",
+     [username, email, password, userType], function(err, resp) {
+        if (err) {
+            console.log("Error " + err);
+        }
+        res.json(resp);
+     });
+});
+
+router.post("/addCityOfficial", function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    con.query("INSERT INTO CityOfficial VALUES (?, ?, ?, ?, ?)",
+     [username, req.body.title, false, req.body.city, req.body.state], function(err, resp) {
+        if (err) {
+            console.log("Error " + err);
+        }
+        res.json(resp);
+     });
+});
+
+
+/***************************************************************************** 
+                        LOGIN ENDPOINTS
+******************************************************************************/
+router.get("/getUser", function(req, res) {
+    var username = req.query.username;
+    var password = req.query.password;
+    var obj = { status: false};
+    con.query("SELECT * FROM User WHERE Username = ? and Password = ?",
+        [username, password], function(err, response){
+        if (err)
+            res.json(err);
+        res.json(response);
+    });
+});
+
+
+
+/***************************************************************************** 
+                        DROPDOWN MENU ENDPOINTS
+******************************************************************************/
 router.get("/cityList", function(req, res) {
     con.query('SELECT DISTINCT City FROM CityState ORDER BY City',function(err,rows) {
         if(err)
@@ -125,6 +133,10 @@ router.get("/typeList", function(req, res) {
     });
 });
 
+/***************************************************************************** 
+                        CITY SCIENTIST ENDPOINTS
+******************************************************************************/
+
 router.post("/addDataPoint", function(req, res) {
     var datetime = req.body.datetime;
     var value = req.body.value;
@@ -150,6 +162,9 @@ router.post("/addPOILocation", function(req, res) {
      });
 });
 
+/***************************************************************************** 
+                        ADMIN ENDPOINTS
+******************************************************************************/
 router.get("/pendingAccounts", function(req, res) {
     con.query('SELECT Username, Email, Title, City, State FROM CityOfficial NATURAL JOIN User WHERE Approved = false',function(err,rows) {
         if(err)
@@ -206,6 +221,9 @@ router.post("/rejectDataPoint", function(req, res) {
      });
 });
 
+/***************************************************************************** 
+                        CITY OFFICIAL ENDPOINTS
+******************************************************************************/
 router.get("/filterPOIs", function(req, res) {
     var and = "";
     var query = "";
@@ -237,146 +255,5 @@ router.get("/filterPOIs", function(req, res) {
         res.json(rows);
     });
 });
-/* !!!!!!!!!      EVERYTHING BELOW THIS LINE IS FROM 2340     !!!!!!!!!! */
-
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works!');
-});
-
-router.get("/userList", function(req, res) {
-        con.query('SELECT * FROM User',function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-            res.json(rows);
-         });
-    });
-
-
-    
-
-    router.post("/addUser", function(req, res) {
-        var username = req.body.username;
-        var password = req.body.password;
-        var email = req.body.email;
-        var userType = req.body.userType;
-        con.query("INSERT INTO User VALUES (?, ?, ?, ?)",
-         [username, email, password, userType], function(err, resp) {
-            if (err) {
-                console.log("Error " + err);
-                res.json(resp);
-            }
-            res.json(resp);
-         });
-    });
-
-    router.put("/editUser", function(req, res) {
-        var firstName = req.body.firstName;
-        var lastName = req.body.lastName;
-        var username = req.body.username;
-        var password = req.body.password;
-        var email = req.body.email;
-        var accountType = req.body.accountType;
-        var address = req.body.address;
-        var title = req.body.title;
-        con.query("UPDATE userInfo SET firstName = ?," +
-            " lastName = ?, email = ?, password = ?, address = ?, " +
-            "title = ?" +
-            " WHERE " +
-            "username = ?", [firstName, lastName, email, password, address,
-            title, username], function(err, resp){
-                if (err) {
-                    res.json(resp);
-                    console.log("Error");
-                } else {
-                    res.json(resp);
-                    console.log("Success");
-                }
-        });
-    });
-
-
-
-    /* ------------------------------------------------
-    Source Report Table Endpoints
-    --------------------------------------------------*/
-    router.get("/sourceReportList", function(req, res) {
-        con.query('SELECT * FROM sourceReportInfo',function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-            res.json(rows);
-         });
-    });
-
-    router.get("/sourceReportCount", function(req, res) {
-        con.query('SELECT COUNT(*) as rowcount FROM sourceReportInfo',function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-            res.json(rows);
-         });
-    });
-
-    router.put("/addSourceReport", function(req, res) {
-        var date = req.body.date;
-        var reportNumber = req.body.reportNumber;
-        var username = req.body.username;
-        var longitude = req.body.longitude;
-        var latitude = req.body.latitude;
-        var type = req.body.type;
-        var condition = req.body.condition;
-        con.query("INSERT INTO sourceReportInfo SET date = ?, reportNumber = ?, username = ?, "
-            + "longitude = ?, latitude = ?, type = ?, cond = ?",
-            [date, reportNumber, username, longitude, latitude, type, condition],
-            function(err, resp) {
-            if (err) {
-                console.log("Error " + err);
-            }
-            console.log(resp);
-            res.json(resp);
-         });
-    });
-
-
-    /* ------------------------------------------------
-    Quality Table Endpoints
-    --------------------------------------------------*/
-    router.get("/qualityReportList", function(req, res) {
-        con.query('SELECT * FROM qualityReportInfo',function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-            res.json(rows);
-         });
-    });
-
-    router.get("/qualityReportCount", function(req, res) {
-        con.query('SELECT COUNT(*) as rowcount FROM qualityReportInfo',function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-            res.json(rows);
-         });
-    });
-
-    router.put("/addQualityReport", function(req, res) {
-        var date = req.body.date;
-        var reportNumber = req.body.reportNumber;
-        var username = req.body.username;
-        var longitude = req.body.longitude;
-        var latitude = req.body.latitude;
-        var condition = req.body.condition;
-        var virus = req.body.virus;
-        var chem = req.body.chem;
-        con.query("INSERT INTO qualityReportInfo SET date = ?, reportNumber = ?, username = ?, "
-            + "longitude = ?, latitude = ?, cond = ?, virus = ?, chem = ?",
-            [date, reportNumber, username, longitude, latitude, condition, virus, chem],
-            function(err, resp) {
-            if (err) {
-                console.log("Error");
-            }
-            res.json(resp);
-         });
-    });
-
-
-
 
 module.exports = router;
